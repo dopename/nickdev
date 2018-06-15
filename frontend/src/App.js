57c83ad54fbd3661a3f6cf02f100e714ef46530d
unknown <marini.nick@gmail.com>
2018-06-14 23:08:48 -0700

    Table lul

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from '/components/table.jsx'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           New boilerplate? I think so.
         </p>
+        <Table />
       </div>
     );
   }
---
+++ frontend/src/components/table.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Table extends Component {
+	render() {
+		return (
+			<table>
+				<tr>
+					<th>This is a table</th>
+				</tr>
+				<tr>
+					<td>I don't know I'm just making a table</td>
+					<td>So here's some stuff</td>
+				</tr>
+			</table>
+		)
+	}
+}
