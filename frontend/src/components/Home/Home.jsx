4fc5dbc2ab53414c68e57092feca5346ec3772c2
unknown <marini.nick@gmail.com>
2018-06-15 23:05:44 -0700

    More initial setup (navbar and the likes)

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -8,9 +8,13 @@ export default class Home extends Component {
 
 		this.state = {
 			logged_in:localStorage.getItem('token') ? true : false,
+			user:localStorage.getItem('user') ? localStorage.getItem('user') : false,
+			loginPage:false,
 		}
 
 		this.handleLogout = this.handleLogout.bind(this);
+		this.handleLogin = this.handleLogin.bind(this);
+		this.toggleLoginPage = this.toggleLoginPage.bind(this);
 	}
 
 	componentDidMount() {
@@ -22,11 +26,32 @@ export default class Home extends Component {
 		this.setState({logged_in:false});
 	}
 
+	handleLogin(e, data) {
+		e.preventDefault();
+		fetch('http://www.nickstestdev.com/token-auth/', {
+		  method:'POST',
+		  headers: {
+			'Content-Type': 'application/json'
+		  },
+		  body:JSON.stringify(data)
+		})
+		.then(response => response.json())
+		.then(json => {
+		  console.log(json);
+		  localStorage.setItem('token', json.token);
+		  this.setState({loginPage:true});
+		})
+	}
+
+	toggleLoginPage() {
+		this.setState({loginPage:!this.state.loginPage});
+	}
+
 	render() {
 		return(
 			<div className="container-fluid">
-					<TopNav />
-					<Content />
+				<TopNav handleLogout={this.handleLogout} toggleLoginPage={this.toggleLoginPage} />
+				<Content loginPage={this.state.loginPage} handleLogin={this.handleLogin} />
 			</div>
 		)
 	}
@@ -44,7 +69,7 @@ class TopNav extends Component {
 					<ul className="list-inline text-center">
 						<li className="list-inline-item mx-2">Home</li>
 						<li className="list-inline-item mx-2">/</li>
-						{this.props.logged_in ? <li className="list-inline-item mx-2" onClick={e => this.props.handleLogout(e)}>Logout</li> : <li className="list-inline-item mx-2">Login</li> }
+						{this.props.logged_in ? <li className="list-inline-item mx-2" onClick={e => this.props.handleLogout(e)}>Logout</li> : <li className="list-inline-item mx-2" onClick={() => this.props.toggleLoginPage()}>Login</li> }
 					</ul>
 				</div>
 			</div>
@@ -53,16 +78,28 @@ class TopNav extends Component {
 }
 
 class Content extends Component {
+	constructor(props) {
+		super(props)
+	}
+
 	render() {
+		let contentBody = (
+					<div className="text-center">
+						<h1>Welcome to my Development Environment!</h1>
+						<p>This shit gon' get weird.</p>
+					</div>
+			)
+
+		if (this.props.loginPage) {
+			contentBody = <LoginPage handleLogin={this.props.handleLogin} />
+		}
+
 		return (
 			<div className="row my-3">
 				<div className="col-lg-2">
 				</div>
 				<div className="col-lg-8">
-					<div className="text-center">
-						<h1>Welcome to my Development Environment!</h1>
-						<p>This shit gon' get weird.</p>
-					</div>
+					{contentBody}
 				</div>
 				<div className="col-lg-2">
 				</div>
@@ -70,3 +107,5 @@ class Content extends Component {
 		)
 	}
 }
+
+
---
+++ frontend/src/components/Home/LoginPage/LoginPage.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props)
+
+     this.state = {
+        username: '',
+        password: ''
+      }
+      this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(prevstate => {
+      const newState = { ...prevstate };
+      newState[name] = value;
+      return newState;
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={e => this.props.handleLogin(e, this.state)}>
+        <div className="text-center">
+          <div className="row my-3">
+            <div className="col-lg-12">
+              <h2>Log In</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-3">
+            </div>
+            <div className="col-lg-6">
+              <div className="mt-2">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                  className="form-control"
+                />
+              </div>
+              <div className="my-2">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  className="form-control"
+                />
+              </div>
+              <input type="submit" className="btn btn-lg btn-secondary" />
+            </div>
+            <div className="col-lg-3">
+            </div>
+        </div>
+      </div>
+    </form>
+    );
+  }
+}
+
+export default LoginForm;
