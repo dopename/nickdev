b652e7dc7678caf18ca2f732f1a04e99dd69d4ac
unknown <marini.nick@gmail.com>
2018-06-15 19:02:20 -0700

    put api underneath main

--- nickdev/urls.py
+++ nickdev/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', api.urls),
+    path('api/', include("main.api.urls")),
     path('', TemplateView.as_view(template_name="index.html")),
 ]
