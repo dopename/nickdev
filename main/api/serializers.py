59d270777888a9f96fb30dfb6a732dbf75afc16f
unknown <marini.nick@gmail.com>
2018-06-15 20:39:54 -0700

    more changes for default configuration

--- main/api/serializers.py
+++ main/api/serializers.py
@@ -3,9 +3,11 @@ from main.models import CustomUser
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+	username = serializers.CharField(source='user.username', read_only=True)
+
 	class Meta:
 		model = CustomUser
 		fields = [
 			'pk',
-			'user.username'
+			'username'
 		]
