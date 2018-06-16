020e7293aa762755056f849128e857a78d123be2
unknown <marini.nick@gmail.com>
2018-06-15 22:45:40 -0700

    More initial setup (navbar and the likes)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Table from './components/table.jsx'
+import Home from './components/Home/Home.jsx'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          New boilerplate? I think so.
-        </p>
-        <Table />
+        <Home />
       </div>
     );
   }
---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import './Home.css'
+
+export default class Home extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			logged_in:localStorage.getItem('token') ? true : false,
+		}
+
+		this.handleLogout = this.handleLogout.bind(this);
+	}
+
+	componentDidMount() {
+		return null
+	}
+
+	handleLogout(e) {
+		localStorage.removeItem('token');
+		this.setState({logged_in:false});
+	}
+
+	render() {
+		return(
+			<div className="container-fluid">
+				<div className="row">
+					<TopNav />
+					<Content />
+				</div>
+			</div>
+		)
+	}
+}
+
+class TopNav extends Component {
+	constructor(props){
+		super(props)
+	}
+
+	render() {
+		return (
+			<ul className="list-inline text-center">
+				<li className="list-inline-item mx-2"><Link to="/home">Home</Link></li>
+				<li className="list-inline-item mx-2">/</li>
+				{this.props.logged_in ? <li className="list-inline-item mx-2" onClick={e => this.props.handleLogout(e)}>Logout</li> : <li className="list-inline-item mx-2"><Link to={'/login/'}>Login</Link></li> }
+			</ul>
+		)
+	}
+}
+
+class Content extends Component {
+	render() {
+		return (
+			<div className="row my-3">
+				<div className="col-lg-2">
+				</div>
+				<div className="col-lg-8">
+					<div className="text-center">
+						<h1>Welcome to my Development Environment!</h1>
+						<p>This shit gon' get weird.</p>
+					</div>
+				</div>
+				<div className="col-lg-2">
+				</div>
+			</div>
+		)
+	}
+}
