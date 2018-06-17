2377d2bf2e810f9023df2c474d270b481dbabce1
unknown <marini.nick@gmail.com>
2018-06-17 12:04:20 -0700

    Touching up home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -67,7 +67,7 @@ export default class Home extends Component {
 		let sideNav = (
 					<Nav vertical>
 						<NavItem className="text-center">
-							<Button outline className="text-center" onClick={() => this.toggleSideNav()} color="primary"><i class="fa fa-arrows-h"></i></Button>
+							<Button outline className="text-center" onClick={() => this.toggleSideNav()} color={this.state.sideNav ? "success" : "danger"}><i class="fa fa-arrows-h"></i></Button>
 						</NavItem>
 						<hr />
 						<NavItem>
