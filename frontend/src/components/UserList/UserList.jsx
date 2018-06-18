091acaeed734b865b3ae626b14717da0b146142c
unknown <marini.nick@gmail.com>
2018-06-18 12:53:43 -0700

    more model updates

--- frontend/src/components/UserList/UserList.jsx
+++ frontend/src/components/UserList/UserList.jsx
@@ -9,6 +9,7 @@ export default class UserList extends Component {
 			user_list:[],
 			user:false,
 			mode:'view',
+			activeList:false,
 		}
 	}
 
@@ -55,6 +56,15 @@ export default class UserList extends Component {
 		})
 	}
 
+	toggleActiveList(pk) {
+		if (this.state.activeList === pk) {
+			this.setState({activeList:false});
+		}
+		else {
+			this.setState({activeList:pk});
+		}
+	}
+
 	toggleView() {
 		var newValue = this.state.mode === 'view' ? 'create' : 'view';
 		this.setState({mode:newValue});
@@ -64,16 +74,20 @@ export default class UserList extends Component {
 		var renderList = [];
 
 		this.state.user_list.map((ul) => {
-			renderList.push(<li>{ul.list_title}</li>);
+			renderList.push(<li key={ul.pk} onClick={() => this.toggleActiveList(ul.pk)} className={this.state.activeList === ul.pk ? "list-group-item active" : "list-group-item"}>{ul.list_title}</li>);
 		});
 
 		return (
-			<div>
-				<h3>Existing Lists</h3>
-				<ul>
-					{renderList}
-				</ul>
-				<h3>Click <p onClick={() => this.toggleView()}>here</p> to create a new list</h3>
+			<div className="row">
+				<div className="col-lg-6">
+					<h3>Existing Lists</h3>
+					<ul className="list-group">
+						{renderList}
+					</ul>
+				</div>
+				<div className="col-lg-6">
+					<h3>Click <p onClick={() => this.toggleView()}>here</p> to create a new list</h3>
+				</div>
 			</div>
 		)
 	}
