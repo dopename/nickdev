7cf4a5674f860b0ca39f9316ed47f81cab945d80
unknown <marini.nick@gmail.com>
2018-06-19 14:44:15 -0700

    fixing the refresh

--- frontend/src/components/UserList/ListItems/ListItems.jsx
+++ frontend/src/components/UserList/ListItems/ListItems.jsx
@@ -108,11 +108,10 @@ export default class ListItems extends Component {
 		var renderItems = [];
 
 		this.state.items.map((item) => {
-			console.log(item, this.state.accordianDisplay);
 			renderItems.push(
 				<li className="list-group-item">
 					<h4 key={item.pk} onClick={() => this.toggleAccordianClick(item.pk)} className="pointer-hand">{item.item_title}</h4>
-					<h4 key={"accordian" + item.pk} className={this.state.accordianDisplay === item.pk ? "d-none" : "d-block"}>{item.description}</h4>
+					<h4 key={"accordian" + item.pk} className={this.state.accordianDisplay === item.pk ? "d-block" : "d-none"}>{item.description}</h4>
 				</li>
 				)
 		}, this)
