eeebe7fd37519e2d6920e419f247fd85a6aaa4a0
unknown <marini.nick@gmail.com>
2018-06-19 13:52:07 -0700

    fixing the refresh

--- frontend/src/components/UserList/UserList.jsx
+++ frontend/src/components/UserList/UserList.jsx
@@ -18,6 +18,7 @@ export default class UserList extends Component {
 		this.infoFromToken = this.infoFromToken.bind(this)
 		this.submitDeleteList = this.submitDeleteList.bind(this);
 		this.submitCreateList = this.submitCreateList.bind(this);
+		this.fetchUserList = this.fetchUserList.bind(this);
 	}
 
 	componentDidMount() {
