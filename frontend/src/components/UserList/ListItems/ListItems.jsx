4dd291075eb40538d7de01c49b85bd4d4353e8fe
unknown <marini.nick@gmail.com>
2018-06-19 16:57:12 -0700

    fixing the refresh

--- frontend/src/components/UserList/ListItems/ListItems.jsx
+++ frontend/src/components/UserList/ListItems/ListItems.jsx
@@ -146,7 +146,7 @@ export default class ListItems extends Component {
 			renderItems.push(
 				<li className="list-group-item">
 					<h4 key={item.pk}>
-						<i onClick={() => this.toggleAccordianClick(item.pk)} className="text-info float-left fa fa-arrows pointer-hand"></i>{item.item_title}<i onClick={() => this.toggleEdit(item.pk)} className="fa fa-penic text-warning pointer-hand float-right"></i><i onClick={() => this.deleteListItem(item.pk)} className="fa fa-close text-danger pointer-hand float-right"></i>
+						<i onClick={() => this.toggleAccordianClick(item.pk)} className="text-info float-left fa fa-arrows pointer-hand"></i>{item.item_title}<i onClick={() => this.toggleEdit(item.pk)} className="fa fa-pencil text-warning pointer-hand float-right"></i><i onClick={() => this.deleteListItem(item.pk)} className="fa fa-close text-danger pointer-hand float-right"></i>
 					</h4>
 					<h4 key={"accordian" + item.pk} className={this.state.accordianDisplay === item.pk ? "d-block text-left" : "d-none"}>{item.description}</h4>
 				</li>
