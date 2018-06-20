daf7a5ebba8f3c8316ae21fcd024ba9dd179bd83
unknown <marini.nick@gmail.com>
2018-06-19 18:52:39 -0700

    fixing the refresh

--- frontend/src/components/UserList/UserList.jsx
+++ frontend/src/components/UserList/UserList.jsx
@@ -135,13 +135,13 @@ export default class UserList extends Component {
 		return (
 			<div>
 				<div className="row my-2 border-bottom border-dark">
-					<div class="col-lg-4 text-center mt-1">
+					<div class="col-lg-4 text-center my-1">
 						<Button outline className={this.state.mode === "create" ? "active" : null} color="success" size="lg" onClick={() => this.toggleView('create')}>Create a list</Button>
 					</div>
-					<div class="col-lg-4 text-center mt-1">
+					<div class="col-lg-4 text-center my-1">
 						<Button outline className={this.state.mode === "delete" ? "active" : null} color="danger" size="lg" onClick={() => this.toggleView('delete')}>Delete a list</Button>
 					</div>
-					<div class="col-lg-4 text-center mt-1">
+					<div class="col-lg-4 text-center my-1">
 						<Button outline className={this.state.mode === "view" ? "active" : null} color="info" size="lg" onClick={() => this.toggleView('view')}>View lists</Button>
 					</div>
 				</div>
