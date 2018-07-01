6e818b33eb0d9fd61a443ce8cda0888e972212a4
unknown <marini.nick@gmail.com>
2018-07-01 01:00:30 -0700

    Pushing new models, serializers and views for new PM tool

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from main.models import CustomUser, UserList, ListItem
+from main.models import CustomUser, UserList, ListItem, Project, Phase, Objective
 
 admin.site.register(CustomUser)
 admin.site.register(UserList)
 admin.site.register(ListItem)
+admin.site.register(Project)
+admin.site.register(Phase)
+admin.site.register(Objective)
