174def9c91f261a2d81a0830b1959c7d1c3e2d15
nmarini <marini.nick@gmail.com>
2018-07-02 11:37:47 -0700

    modularizing

--- frontend/src/components/ProjectManagement/ProjectManagement.jsx
+++ frontend/src/components/ProjectManagement/ProjectManagement.jsx
@@ -4,24 +4,37 @@ import { Button } from 'reactstrap';
 export default class ProjectManagement extends Component {
 	constructor(props) {
 		super(props)
+
+		this.state = {
+			view:'home',
+		}
+	}
+
+	changeView(view) {
+		this.setState({view:view});
 	}
 
 	render() {
 
 		return (
 			<div>
-				<h1>This is your Project Management home screen</h1>
-				<div className="row">
-					<div className="col-6">
-						<h3>Working on an existing project?</h3>
-						<Button outline size="md" color="success">Click here</Button>
-					</div>
-					<div className="col-6">
-						<h3>Starting a new project?</h3>
-						<Button outline size="md" color="success">Click here</Button>
-					</div>
-				</div>
+				{this.state.view === 'home' ? homeScreen : null }
+				{this.state.view === 'existing' ? existingProjects : null }
+				{this.state.view === 'new' ? newProject : null }
 			</div>
 		)
 	}
 }
+
+const homeScreen = ( 
+				<div className="row">
+					<div className="col-6">
+						<h2>Working on an existing project?</h2>
+						<Button outline size="md" color="success" onClick={ () => this.changeView('existing') }>Click here</Button>
+					</div>
+					<div className="col-6">
+						<h2>Starting a new project?</h2>
+						<Button outline size="md" color="success" onClick={ () => this.changeView('new') }>Click here</Button>
+					</div>
+				</div>
+	)
