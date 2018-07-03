239c737cd04c9f1285dab9f07106fbc002e91835
unknown <marini.nick@gmail.com>
2018-07-02 21:14:20 -0700

    playing around with animations

--- frontend/src/components/ProjectManagement/ProjectManagement.jsx
+++ frontend/src/components/ProjectManagement/ProjectManagement.jsx
@@ -341,7 +341,7 @@ class PhaseObjectives extends Component {
 	}
 
 	handleChange(event) {
-		this.setState( [event.target.name] : event.target.value})
+		this.setState({[event.target.name] : event.target.value});
 	}
 
 	render() {
