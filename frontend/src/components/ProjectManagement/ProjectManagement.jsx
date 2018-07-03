ab6503a711de2414eede4a40b099e178ddd5c9fe
nmarini <marini.nick@gmail.com>
2018-07-03 10:14:03 -0700

    Doing some interesting things, CSS Transition

--- frontend/src/components/ProjectManagement/ProjectManagement.jsx
+++ frontend/src/components/ProjectManagement/ProjectManagement.jsx
@@ -587,7 +587,7 @@ class ObjectiveInfo extends Component {
 				<p className="my-1 px-2 text-left"><strong>Due Date:</strong> {this.props.o.due_date}</p>
 				<ButtonGroup className="mb-2">
 					<Button outline size="md" color="warning" onClick={() => this.toggleEditable()}>Edit Objective</Button>
-					<Button outline size="md" color="danger" onclick={() => this.props.deleteObjective(o.pk, o.title)}>Delete Objective</Button>
+					<Button outline size="md" color="danger" onclick={() => this.props.deleteObjective(this.props.o.pk, this.props.o.title)}>Delete Objective</Button>
 					{this.props.o.completed ? <Button outline size="md" onClick={() => {this.props.completeObjective(this.props.o, true)} } color="secondary">Mark Incomplete</Button> : <Button outline size="md" onClick={() => {this.props.completeObjective(this.props.o, true)} } color="success">Mark Complete</Button>}
 				</ButtonGroup>
 			</div>
